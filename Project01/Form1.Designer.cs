04af881f9b5e96f6556ea095d4d55a0cba584b45
RenGail5511 <Donutzaba@gmail.com>
2016-12-02 21:59:08 +0700

    Project RenGaiL

---
+++ Project01/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Project01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnFull = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(167, 560);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 112;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Location";
+            this.columnHeader2.Width = 83;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 578);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(49, 27);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(67, 578);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(53, 27);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(126, 578);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(53, 27);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(188, 12);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(636, 609);
+            this.axWindowsMediaPlayer1.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnFull
+            // 
+            this.btnFull.Location = new System.Drawing.Point(12, 611);
+            this.btnFull.Name = "btnFull";
+            this.btnFull.Size = new System.Drawing.Size(167, 22);
+            this.btnFull.TabIndex = 5;
+            this.btnFull.Text = "Fullscrean";
+            this.btnFull.UseVisualStyleBackColor = true;
+            this.btnFull.Click += new System.EventHandler(this.btnFull_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(836, 633);
+            this.Controls.Add(this.btnFull);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(852, 672);
+            this.MinimumSize = new System.Drawing.Size(852, 672);
+            this.Name = "Form1";
+            this.Text = "Media Player";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnAdd;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnFull;
+    }
+}
+
---
+++ Project01/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            //open dialog and select a file name
+            Stream MyListPlay = null;  //สร้าง ลิสเพล ขึ้นมาเพื่อใช้งาน
+            openFileDialog1 = new OpenFileDialog();//สร้างไฟล์ไดอาล็อค
+            openFileDialog1.Filter = "All FILES (*.*)|*.*";//ฟิลเตอร์เพื่อจับนามสกุลไฟล์
+            openFileDialog1.Multiselect = true;//ทำให้สามารถเพิ่มไฟล์ได้พร้อมกันหลายๆไฟล์
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms. DialogResult.OK)
+            {
+
+
+                if ((MyListPlay = openFileDialog1.OpenFile()) != null)//สร้างเงื่อนไขเพื่อเพิ่มไฟล์
+                {
+                    using (MyListPlay)
+                    {
+                        string[] filePath = openFileDialog1.FileNames;
+                        string[] filename = openFileDialog1.SafeFileNames;
+
+                        for (int i = 0; i < openFileDialog1.SafeFileNames.Count(); i++)//จัดเก็บไฟล์ จะใช้ลูปเพื่อให้เพิ่มไฟล์ได้หลายๆรอบ
+                        {
+                            string[] saLvwItem = new string[2];
+                            saLvwItem[0] = filename[i];
+                            saLvwItem[1] = filePath[i];
+                            ListViewItem lvi = new ListViewItem(saLvwItem);//นำมาจัดเก็บไว้ใน ลิสวิว
+                            listView1.Items.Add(lvi);
+                        }
+                    }
+                }
+            }
+        }
+                   
+        private void btnPlay_Click(object sender, EventArgs e)
+        {   
+            // play all media on playlist
+            WMPLib.IWMPPlaylist playlist = axWindowsMediaPlayer1.playlistCollection.newPlaylist("myplaylist");//สั่งให้มีเดียเพลเยอร์เล่นไฟล์
+            WMPLib.IWMPMedia media;
+
+            for (int i = 0; i < listView1.Items.Count; i++)//ลูปเพื่อให้เล่นไฟล์ทั้งหมดแบบต่อเนื่อง
+            {
+
+                int j = 1;
+                media = axWindowsMediaPlayer1.newMedia(listView1.Items[i].SubItems[j].Text);
+                playlist.appendItem(media);
+                j++;
+                axWindowsMediaPlayer1.currentPlaylist = playlist;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //double click on a song for play
+            {
+                string selectedFile = listView1.FocusedItem.SubItems[1].Text;//สร้างขึ้นเพื่อให้สามารถดับเบิ้ลคลิกที่ไฟล์เพื่อเล่นได้
+                axWindowsMediaPlayer1.URL = @selectedFile;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear playlist of data
+            listView1.Items.Clear();
+        }
+
+        private void btnFull_Click(object sender, EventArgs e)
+        {
+            if (axWindowsMediaPlayer1.playState == WMPLib.WMPPlayState.wmppsPlaying)//สามารถทำให้ตัวมีเดียเพลเยอร์สามารถเข้า/ออกโหมดฟูลสกินได้ ในกรณีที่เล่นไฟอยู่
+            { axWindowsMediaPlayer1.fullScreen = true; }
+        }
+      }
+    }
+
+
